3be8b2e58aefc240bc0c8d77ddc0ba72695e709d
dongseup_lim <dongseup83@gmail.com>
2022-11-23 17:49:03 +0900

    최초 세팅 커밋

---
+++ src/main/java/com/noone/spring_student_records/SpringStudentRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.noone.spring_student_records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noone/spring_student_records/SpringStudentRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noone.spring_student_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
